fe69e3e411e0cf85eb89b648fb900c38e882c7c3
Imre Kovács <2daymc@gmail.com>
2021-05-12 20:13:02 +0200

    Initial commit of the board

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+import hu.maven.Components.BoardGameApplication;
+import javafx.application.Application;
+
+public class App {
+
+    public static void main(String[] args) {
+        Application.launch(BoardGameApplication.class, args);
+    }
+
+}
---
+++ src/main/java/hu/maven/Components/BoardGameController.java
@@ -0,0 +1,47 @@
+package hu.maven.Components;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import static javafx.scene.paint.Color.TRANSPARENT;
+
+public class BoardGameController {
+
+    @FXML
+    private GridPane board;
+
+    @FXML
+    private void initialize() {
+        for (int i = 0; i < board.getRowCount(); i++) {
+            for (int j = 0; j < board.getColumnCount(); j++) {
+                var square = createSquare();
+                board.add(square, j, i);
+            }
+        }
+    }
+
+    private StackPane createSquare() {
+        var square = new StackPane();
+        square.getStyleClass().add("square");
+        var piece = new Circle(50);
+        piece.setFill(Color.BLACK);
+        square.getChildren().add(piece);
+        square.setOnMouseClicked(this::handleMouseClick);
+        return square;
+    }
+
+    @FXML
+    private void handleMouseClick(MouseEvent event) {
+        var square = (StackPane) event.getSource();
+        var row = GridPane.getRowIndex(square);
+        var col = GridPane.getColumnIndex(square);
+        System.out.printf("Click on square (%d,%d)\n", row, col);
+        var coin = (Circle) square.getChildren().get(0);
+        coin.setFill(TRANSPARENT);
+    }
+
+}
