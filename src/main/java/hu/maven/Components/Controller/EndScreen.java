9253c19d79180bd878d902acc6326b4cb1568105
Imre Kovács <2daymc@gmail.com>
2021-05-17 10:26:22 +0200

    First iteration of the End Screen

---
+++ src/main/java/hu/maven/Components/Controller/EndScreen.java
@@ -0,0 +1,23 @@
+package hu.maven.Components.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EndScreen {
+
+    public TextField winner;
+
+    public void winnerPrinted(){
+        winner.setText();
+    }
+
+}
